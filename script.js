b67bdc1b727f0bf44762ef13dea0c9cddd0bb998
Iman Firdaus <smuhd827@gmail.com>
2021-11-30 09:44:47 +0800

    latihan

---
+++ script.js
@@ -0,0 +1,26 @@
+function gred (markah,lokasi){
+    document.getElementById(lokasi).innerText=markah
+}
+/*if(number<21){
+        
+        document.getElementById("resulta").innerHTML="E"
+    }
+    else if(number<41){
+
+        document.getElementById("resulta").innerHTML="D"
+        
+    }
+    else if(number<61){
+        
+        document.getElementById("resulta").innerHTML="C"
+    }
+    else if(number<81){
+
+        document.getElementById("resulta").innerHTML="B"
+    }
+    else
+
+    document.getElementById("resulta").innerHTML="A"
+
+*/   
+
